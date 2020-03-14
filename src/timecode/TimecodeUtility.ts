752c1c126d7e9c97c713eae55ef2c3611160f895
Christian Kernstock <ckrnstck@hideo.fritz.box>
2020-03-14 13:15:06 +0100

    add fromString method

--- src/timecode/TimecodeUtility.ts
+++ src/timecode/TimecodeUtility.ts
@@ -8,4 +8,36 @@ export class TimecodeUtility
   {
     return new Timecode(obj.hours, obj.minutes, obj.seconds, obj.frames);
   }
+
+  public static fromString(input: string): ITimecode
+  {
+    const regExFramesDot = /^\d*:\d*:\d*\.\d*$/ig;
+    const regExFramesColon = /^\d*:\d*:\d*\:\d*$/ig;
+
+    const colonParts = input.split(':');
+
+    let hours: number = parseInt(colonParts[0]);
+    let minutes: number = parseInt(colonParts[1]);
+    let seconds: number = parseInt(colonParts[2]);
+    let frames = 0;
+
+    if (input.match(regExFramesDot) != null)
+    {
+      const dotIndex = input.indexOf('.');
+      frames = parseInt(input.substr(dotIndex + 1));
+    }
+    else if (input.match(regExFramesColon) != null)
+    {
+      frames = parseInt(colonParts[3]);
+    }
+
+    const obj = {
+      hours,
+      minutes,
+      seconds,
+      frames
+    };
+
+    return TimecodeUtility.fromObject(obj);
+  }
 }
